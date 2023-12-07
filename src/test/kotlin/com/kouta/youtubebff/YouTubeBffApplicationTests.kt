4b87cfacbfafcf4f997f84a113e0ad3de54d2e32
mattukouta <bb1016181@gmail.com>
2023-12-08 02:52:15 +0900

    pom管理に変更

--- src/test/kotlin/com/kouta/youtubebff/YouTubeBffApplicationTests.kt
+++ src/test/kotlin/com/kouta/youtubebff/YouTubeBffApplicationTests.kt
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class YouTubeBffApplicationTests {
+class YoutubebffApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+	@Test
+	fun contextLoads() {
+	}
 
 }
